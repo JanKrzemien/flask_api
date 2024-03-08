cdea09d90f95d2f46dcbc24d81d928672b47139e
JanKrzemien <jan.krzemien.6@gmail.com>
2024-03-08 16:27:39 +0100

    added logic for creating db

--- flaskr/db.py
+++ flaskr/db.py
@@ -3,17 +3,44 @@ import click
 from flask import current_app, g
 
 def get_db():
+    """connects to database if connection wasn't established yet
+
+    Returns:
+        special type g: special object used for db connection
+    """
     if 'db' not in g:
         g.db = sqlite3.connect(
-            current_app.config('DATABASE'),
+            current_app.config['DATABASE'],
             detect_types=sqlite3.PARSE_DECLTYPES
         )
         g.db.row_factory = sqlite3.Row
         
     return g.db
 
-def close_db():
+
+def close_db(e=None):
+    """close connection to db"""
     db = g.pop('db', None)
     
     if db is not None:
         db.close()
+
+
+def init_db():
+    """create db from sql script file"""
+    db = get_db()
+    
+    with current_app.open_resource('schema.sql') as file:
+        db.executescript(file.read().decode('utf8'))
+
+
+@click.command('init-db')
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    init_db()
+    click.echo('Initialized the database.')
+
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -15,20 +15,23 @@ CREATE TABLE status (
     color VARCHAR(25) NOT NULL
 );
 
-CREATE TABLE model {
+CREATE TABLE model (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name VARCHAR(75) NOT NULL,
     release_date DATE NOT NULL,
     specs VARCHAR(1000)
-};
+);
 
 CREATE TABLE device (
-    serial_number VARCHAR(20) PRIMARY KEY AUTOINCREMENT,
-    FOREIGN KEY (model_id) REFERENCES model (id) NOT NULL,
+    serial_number VARCHAR(20) PRIMARY KEY,
+    model_id INTEGER NOT NULL,
     production_date DATE NOT NULL,
     last_serviced DATE,
     battery DECIMAL(2, 2),
-    FOREIGN KEY (status_id) REFERENCES status (id) NOT NULL,
-    FOREIGN KEY (location_id) REFERENCES location (id),
-    other_info VARCHAR(200)
+    status_id INTEGER NOT NULL,
+    location_id INTEGER,
+    other_info VARCHAR(200),
+    FOREIGN KEY (model_id) REFERENCES model (id),
+    FOREIGN KEY (status_id) REFERENCES status (id),
+    FOREIGN KEY (location_id) REFERENCES location (id)
 );
